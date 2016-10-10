7a077bbf1a662342fd80730b5f4f5d84ca53cf2f
Arunas Mazeika <arunas@timble.net>
2016-10-10 16:38:16 +0200

    Issue #1: Update copyright headers

--- install.php
+++ install.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     LOGman
- * @copyright   Copyright (C) 2011 - 2015 Timble CVBA. (http://www.timble.net)
+ * @copyright   Copyright (C) 2011 - 2016 Timble CVBA. (http://www.timble.net)
  * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
  * @link        http://www.joomlatools.com
  */
